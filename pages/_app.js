067e30714ded2c16e52189e560930d591525acb2
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-21 08:52:12 +0700

    Transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import Transition from "../components/Transition";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     // lun lun thấy component Layout
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
