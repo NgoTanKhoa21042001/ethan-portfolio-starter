a96386df1da653f04240f5f47d079e13b012e2b1
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-24 21:25:57 +0700

    contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,31 @@
+import { BsArrowRight } from "react-icons/bs";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30">
+      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
+        {/* text and form*/}
+        <div className="flex flex-col w-full max-w-[700px]">
+          <h2 className="h2 text-center mb-12">
+            Let's <span>connect.</span>
+          </h2>
+          {/* form */}
+          <form className="flex-1 flex flex-col gap-6 w-full mx-auto">
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="text" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder="message" className="textarea"></textarea>
+
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 flex items-center justify-center">
+              <span>Let's talk</span>
+              <BsArrowRight />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
