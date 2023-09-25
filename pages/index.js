d96fb95a21c84bc66505d524790502df53ac3eff
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-09-25 10:32:55 +0700

    contact

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="bg-primary/60 h-full relative">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto z-50">
           {/* title */}
 
           <motion.h1
